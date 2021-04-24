642abe6fb591227d6d9276ceb29c064a55ff5283
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-04-24 21:34:34 +0600

    Add BootstrapVue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue"
 import app from "./app.vue";
 import router from "./router/index"
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 new Vue({
     el: `#app`,
@@ -10,3 +15,5 @@ new Vue({
     },
     router
 })
+
+window.getSelection().addRange(new Range())
