b2ac11c92d778330368dd338e92f05b5d1b82156
Alibek-dev <54788352+Alibek-dev@users.noreply.github.com>
2021-05-07 15:39:57 +0600

    Изменил main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,53 +2,72 @@
     <v-app id="inspire">
         <v-app-bar
             app
-            color="white"
+            color="#C5CAE9"
             flat
+            height="200"
         >
-            <v-container class="py-0 fill-height">
-                <v-avatar
-                    class="mr-10"
-                    color="grey darken-1"
-                    size="32"
-                ></v-avatar>
+            <v-row class="d-flex flex-column">
+                <v-col>
+                    <v-container>
+                        <h3 id="sub-title">MoonCode</h3>
+                        <span id="main-title">Система автоматизированного тестирования задач</span>
+                    </v-container>
+                </v-col>
+                <v-col>
+                    <v-container class="py-0 fill-height">
+                        <router-link to="/profile">
+                            <v-avatar
+                                class="mr-10"
+                                color="grey darken-1"
+                                size="32"
+                            ></v-avatar>
+                        </router-link>
 
-                <v-btn
-                    v-for="item in items"
-                    :key="item.link"
-                    text
-                    :to="item.link"
-                >
-                    {{ item.title }}
-                </v-btn>
+                        <v-btn
+                            v-for="item in items"
+                            :key="item.link"
+                            text
+                            :to="item.link"
+                        >
+                            {{ item.title }}
+                        </v-btn>
 
-                <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
 
-                <v-responsive max-width="260">
-                    <v-text-field
-                        dense
-                        flat
-                        hide-details
-                        rounded
-                        solo-inverted
-                    ></v-text-field>
-                </v-responsive>
-            </v-container>
+                        <v-responsive max-width="260">
+                            <v-text-field
+                                dense
+                                flat
+                                hide-details
+                                rounded
+                                solo-inverted
+                            ></v-text-field>
+                        </v-responsive>
+                    </v-container>
+                </v-col>
+
+            </v-row>
+
         </v-app-bar>
 
-        <v-main class="grey lighten-3">
+        <v-main app class="grey lighten-3">
             <v-container>
                 <v-row>
                     <v-col cols="2">
                         <v-sheet rounded="lg">
                             <v-list color="transparent">
                                 <v-list-item
-                                    v-for="n in 5"
-                                    :key="n"
+                                    v-for="item in items"
+                                    :key="item.link"
                                     link
+                                    :to="item.link"
                                 >
+                                    <v-list-item-icon>
+                                        <v-icon v-text="item.icon"></v-icon>
+                                    </v-list-item-icon>
                                     <v-list-item-content>
                                         <v-list-item-title>
-                                            List Item {{ n }}
+                                            {{ item.title }}
                                         </v-list-item-title>
                                     </v-list-item-content>
                                 </v-list-item>
@@ -103,5 +122,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+#sub-title {
+    margin: 0;
+    color: darkcyan;
+}
+
+#main-title {
+    margin: 0;
+    padding: 0;
+    color: cadetblue;
+}
 </style>
