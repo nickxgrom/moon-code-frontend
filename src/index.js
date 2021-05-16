3545a8beff4b541b300ff0379d0d4f1382f902dd
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-05-16 15:34:42 +0600

    Uninstall BootstrapVue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import Vue from "vue"
 import app from "./app.vue";
 import router from "./router/index"
-import BootstrapVue from "bootstrap-vue";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from "./plugins/vuetify";
 import dateFilter from "./filters/date.filter.js";
 
 Vue.filter('date', dateFilter)
-Vue.use(BootstrapVue)
 
 new Vue({
     vuetify,
