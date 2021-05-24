77d4c3d84e2d5b361d252c2f273aa7b31e37ee2d
Nick Gromyko <nxgr.dev@gmail.com>
2021-05-24 16:10:51 +0600

    Add NewTask.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
-    <div>
+    <div >
        <h1>Список всех задач</h1>
+        <v-btn
+            @click="addTaskVisible = true"
+        >
+            Добавить задачу
+        </v-btn>
+        <new-task
+            v-if="addTaskVisible"
+            @closeOverlay="addTaskVisible=false"
+        />
     </div>
 </template>
 
 <script>
+    import NewTask from "../components/NewTask.vue";
     export default {
         name: "Home",
+        components: {
+            NewTask,
+        },
+        data() {
+            return {
+                addTaskVisible: false
+            }
+        },
     }
 </script>
 
