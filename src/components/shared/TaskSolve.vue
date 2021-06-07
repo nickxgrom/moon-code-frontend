270a834363c8c55f9c9c11643058d4570fd26af9
Nick Gromyko <nxgr.dev@gmail.com>
2021-06-07 13:46:07 +0600

    Add TaskSolve.vue

---
+++ src/components/shared/TaskSolve.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-sheet>
+        <v-card color="mc-dark-lighten" flat>
+            <v-card-title>Title here</v-card-title>
+            <v-card-subtitle>Задача: </v-card-subtitle>
+
+            <v-card-title class="ml-1 mb-0">Решение:</v-card-title>
+            <v-textarea
+                class="ma-5 mt-0"
+                label="Поле ввода кода"
+                v-model="code"
+                @keydown.tab="onTab"
+                @click="tt=code"
+                v-if="!readonly"
+            />
+
+            <div class="ma-5">
+                <code>Ваш код</code>
+                <pre><code>{{getCode}}</code></pre>
+            </div>
+
+            <v-btn
+                class="ma-5"
+                color="primary"
+                @click="sendSolution"
+            >
+                Отправить решение
+            </v-btn>
+
+            <div v-if="testBlockVisible" class="ma-4">
+                <v-card
+                    v-for="c in 5"
+                    class="my-3"
+                    flat
+                    :key="c"
+                >
+                    <v-progress-linear
+                        color="primary"
+                        height="10"
+                        indeterminate
+                    ></v-progress-linear>
+                    <div class="pa-4">
+                        Task#{{c}}
+                    </div>
+                </v-card>
+            </div>
+            <v-btn
+                v-if="testBlockVisible"
+                @click="$router.push('/')"
+                color="success"
+            >
+                Решать еще задачи
+            </v-btn>
+        </v-card>
+    </v-sheet>
+</template>
+
+<script>
+    export default {
+        name: "TaskSolve",
+        props: {
+            readonly: Boolean
+        },
+        data() {
+            return {
+                code: "",
+                testBlockVisible: false,
+                backToTasksBtnVisible: false,
+            }
+        },
+        computed: {
+            getCode() {
+                return this.code
+            },
+        },
+        methods: {
+            onTab(event) {
+                event.preventDefault()
+                this.code = `${this.code}    `
+            },
+            async sendSolution() {
+                this.testBlockVisible = true
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TaskSolving.vue
@@ -0,0 +1,17 @@
+<template>
+    <task-solve></task-solve>
+</template>
+
+<script>
+    import TaskSolve from "../components/shared/TaskSolve.vue";
+    export default {
+        name: "TaskSolving",
+        components: {
+            TaskSolve,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
